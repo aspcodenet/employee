df53f634736e0db0c6e4fa8680978b97ebe69aea
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-04-10 17:45:04 +0200

    first commit

---
+++ src/main/java/com/example/employee/EmployeeController.java
@@ -0,0 +1,36 @@
+package com.example.employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+    @Autowired
+	private EmployeeRepository employeeRepository;
+
+
+    @GetMapping(path="/employee")
+    @CrossOrigin()
+    List<Employee> getAll(){
+        var l = new ArrayList<Employee>();
+        for(Employee r : employeeRepository.findAll())
+        {
+            l.add(r);
+        }
+        return l;
+    }
+
+    @GetMapping(path="/player/{id}")
+    @CrossOrigin()
+    Employee getSingle(@PathVariable Integer id){
+        return employeeRepository.findById(id).get();
+    }
+
+    
+}
---
+++ src/test/java/com/example/employee/EmployeeTests.java
@@ -0,0 +1,18 @@
+package com.example.employee;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class EmployeeTests {
+	@Test
+	void calculateMonthlySalaryReturnsCorrectValue() {
+		Integer hours = 20;
+		Employee employee = new Employee();	
+		employee.setHourlySalary(100);			
+		employee.setName("Stefan Holmberg");
+		employee.setPersonalNumber("19720803");
+
+		assertThat(employee.calculateMonthlySalary(hours)).isEqualTo(2000);
+
+	}
+
+}
