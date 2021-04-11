ee6b337d5356020cda9cae9d04854c748a87d299
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-04-11 09:00:26 +0200

    Added mocks

--- src/main/java/com/example/employee/EmployeeService.java
+++ src/main/java/com/example/employee/EmployeeService.java
@@ -3,14 +3,17 @@ package com.example.employee;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
 
-    @Autowired
-	private EmployeeRepository employeeRepository;
+	private final EmployeeRepository employeeRepository;
+    
+    EmployeeService(EmployeeRepository employeeRepository) {
+        super();
+        this.employeeRepository = employeeRepository;
+    }
 
     List<Employee> getAll(){
         var l = new ArrayList<Employee>();
